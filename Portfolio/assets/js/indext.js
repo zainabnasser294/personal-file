601609bb03961972175783b333d52e454dd25e8f
Zainab <zainabnasser294@gmail.com>
2024-08-01 02:19:29 +0400

    Add files via upload

---
+++ Portfolio/assets/js/indext.js
@@ -0,0 +1,25 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     function showText(elementId, delay) {
+//       setTimeout(() => {
+//         const element = document.getElementById(elementId);
+//         element.style.display = 'block';
+//         element.classList.add('visible');
+//       }, delay);
+//     }
+  
+//     function hideText(elementId, delay) {
+//       setTimeout(() => {
+//         const element = document.getElementById(elementId);
+//         element.classList.remove('visible');
+//         setTimeout(() => {
+//           element.style.display = 'none';
+//         }, 1000); // This should match the transition duration
+//       }, delay);
+//     }
+  
+//     // Show "Artificial Intelligence" for 3 seconds, then hide it and show "Computer Science"
+//     showText('text1', 0);
+//     hideText('text1', 3000);
+//     showText('text2', 4000);
+//   });
+  
